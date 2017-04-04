d3acf8809dc3a4b808c412577dee37d5004f03d9
CoolCode2 <wcole2222@gmail.com>
2017-04-03 18:40:12 -0600

    solved

--- starter-code/app/js/jquery.app.js
+++ starter-code/app/js/jquery.app.js
@@ -1,42 +1,53 @@
-console.log('twerkin');
+console.log('1twerkin');
 
 
+$(document).ready(function(){
 
 var allCats = $.get('https://ga-cat-rescue.herokuapp.com/api/cats')
 	.done(function(data) {
 		var parsedCats = JSON.parse(data);
-		console.log(parsedCats);
-		 $('#cats').append('<li><span>First Cat!</span></li>');
-		 for (var i = 0; i < parsedCats.length; i++) {
-		 $('#cats').append('<li>'+parsedCats[i].name + " - "+ parsedCats[i].note+'</li>');
+		for (var i = 0; i < parsedCats.length; i++) {
+		 	$('#cats').append('<li>'+parsedCats[i].name + " - "+ parsedCats[i].note+'</li>');
 		 }
-		 
+		 console.log("list appended");
 
 		 $('#new-cat').on("submit", function(event){
-		event.preventDefault();
+		 	event.preventDefault();
+		 	
+			var name = $('#cat-name').val(); //what you write in box
+		
+			var note = $('#cat-note').val(); //what you write in box
+		
 
-		//grab new name and note and append as li
-		var name = $('#cat-name').val();
-		console.log(name);
-		var note = $('#cat-note').val();
-		console.log(note);
+			let newCat = {
+				name: name,
+				note: note
+			};
 
-		let newCat = {
-			name: name,
-			note: note
-		};
+			let newCatString = JSON.stringify(newCat);
+			console.log(newCatString);
 
-		let newCatString = JSON.stringify(newCat);
-		console.log(newCatString);
+			$.ajax({
+				url: 'https://ga-cat-rescue.herokuapp.com/api/cats',
+				type: 'POST',
+				data: newCatString
+			})
+	// $.get('https://ga-cat-rescue.herokuapp.com/api/cats')
+		.done(function(data){
+			var newParsedCat = JSON.parse(data);
+			// var addCat = newParsedCat.pop();
+			// console.log(addCat);
 
-		$.ajax({
-			url: 'https://ga-cat-rescue.herokuapp.com/api/cats',
-			type: 'POST',
-			data: newCatString
-
-			
+			$('#cats').append('<li>'+newParsedCat.name+" - "+newParsedCat.note+'</li>');
 
 		});
+		
+//Catthew McConaughey  Dazed and Cat-Fused
+
+		// get it and append it from the end of the array
+
+		//$('cats').append('<li>'+ parsedCats[i].name +" - "+ parsedCats[i].note);
 
 	});
+});
 });
